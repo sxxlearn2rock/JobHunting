c6b3308f84fd3d225ae24e55fbc500376032ee57
sxxlearn2rock <sxx_uestc@163.com>
2016-03-16 21:08:13 +0800

    huawei201601

--- src/cn/cococode/job/huawei/Internship.java
+++ src/cn/cococode/job/huawei/Internship.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.sound.sampled.SourceDataLine;
 
 public class Internship {
 	public void soda(){
@@ -35,7 +34,41 @@ public class Internship {
 			System.out.println(count);
 		}
 	}
+
+    public static void mingming(){
+        Scanner s = new Scanner(System.in);
+        final int N = s.nextInt();
+        s.nextLine();
+        boolean[] barrel = new boolean[1001];
+        for(int i = 0; i < barrel.length; ++i) barrel[i] = false;
+        for(int i = 0; i < N; ++i){
+            barrel[s.nextInt()] = true;
+            s.nextLine();
+        }
+        for(int i = 1; i < 1001; ++i){
+            if(barrel[i] == true) System.out.println(i);
+        }        
+        
+    }
+	
+    public void toHex(){
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        
+        char[] arr = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int res = 0;
+        for(int i = 2; i < arr.length; ++i){
+        	if (arr[i] >= '0' && arr[i] <= '9' ) {
+				res = res * 16 +  (arr[i] - '0');
+			}else if (arr[i] >= 'A' && arr[i] <= 'F' ) {
+				res = res * 16 +  (arr[i] - 'A' +10);
+			}
+        }
+        System.out.println(res);
+    }
+    
 	public static void main(String[] args) {
-		new Internship().soda();
+		new Internship().toHex();;
 	}
 }
